e732ff4980fbebf229861b97b3303a2caba44643
My Name <knuckostya@gmail.com>
2023-05-27 14:22:16 +0300

    95 ls + CSS

--- src/Components/Main/Profile/AddPost/AddPost.tsx
+++ src/Components/Main/Profile/AddPost/AddPost.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 import {AddNewPostForm} from "../../../../Common/Forms/PostForm";
 import s from "../MyPosts/MyPosts.module.css";
 import userAvatar from "../../../../Common/img/user-avatar.webp";
+import {useAppDispatch, useAppSelector} from "../../../../Redux/store";
+import {AddPostAC} from "../../../../Redux/profileReducer";
 
 const AddPost = () => {
 
 	//остановился
 	//решил разбить отдельно профиль добавление поста и посты чтобы шло всё в ряд по вёрскте
+	const profileInfo = useAppSelector(state => state.profilePage)
+	const dispatch = useAppDispatch()
+
+	const addPost = (postText:string) => {
+		// addPost:(postText:string)=>dispatch(AddPostAC(postText))
+		dispatch(AddPostAC(postText))
+	}
 
 	return (
 		<div>
 			<div className={s.avatar}>
-				<img src={props.profilePageInfo?.photos.small ?
-					props.profilePageInfo?.photos.small : userAvatar} alt="" />
+				<img src={profileInfo.profilePageInfo?.photos.small ?
+					profileInfo.profilePageInfo?.photos.small : userAvatar} alt=""/>
 			</div>
-			<AddNewPostForm addPost={props.addPost}/>
+			<AddNewPostForm addPost={addPost}/>
 		</div>
 	);
 };
